383849030e51d1856dea5fcb0d94e98e773dd779
Kanhadharmik <66319279+Kanhadharmik@users.noreply.github.com>
2020-06-03 21:27:32 +0530

    Add files via upload

---
+++ ka.js
@@ -0,0 +1,8 @@
+$(".slider").click(function(){
+
+  $("body").toggleClass("light-section");
+  $("header").toggleClass("light-header");
+  $(".boxes").toggleClass("light-boxes");
+  $(".details").toggleClass("light-boxes");
+  $(".numbers").toggleClass("light-numbers");
+});
